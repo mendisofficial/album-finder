65898e284eafbb64d6ec995444e1f6f0ba5ad802
Chathusha Mendis <chathushamendis@icloud.com>
2024-04-14 07:43:36 +0530

    added searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-
-import './App.css'
+import "./App.css";
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
 
 function App() {
-  
-
   return (
-   <>
-      <h1>Codédex Project Template</h1>
-    </>
-  )
+    <Container>
+      <InputGroup>
+        <FormControl
+          placeholder="Search For Artist"
+          type="input"
+          aria-label="Search for an Artist"
+          onKeyDown={search} // search function
+            onChange={setSearch} // setSearch
+          style={{
+            width: "300px",
+            height: "35px",
+            borderWidth: "0px",
+            borderStyle: "solid",
+            borderRadius: "5px",
+            marginRight: "10px",
+            paddingLeft: "10px",
+          }}
+        />
+
+        <Button onClick={{}}>Search</Button>
+      </InputGroup>
+    </Container>
+  );
+}
+export default App;
+
+function setSearch() {
+
 }
 
-export default App
+function search() {
+
+}
