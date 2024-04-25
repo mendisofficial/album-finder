41e4f6b0bec0be9917eef07bb103db0747526ab8
Chathusha Mendis <chathushamendis@icloud.com>
2024-04-25 17:19:14 +0530

    artist search okay

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,98 @@
 import "./App.css";
+import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
-import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
 
 function App() {
-  return (
-    <Container>
-      <InputGroup>
-        <FormControl
-          placeholder="Search For Artist"
-          type="input"
-          aria-label="Search for an Artist"
-          onKeyDown={search} // search function
-            onChange={setSearch} // setSearch
-          style={{
-            width: "300px",
-            height: "35px",
-            borderWidth: "0px",
-            borderStyle: "solid",
-            borderRadius: "5px",
-            marginRight: "10px",
-            paddingLeft: "10px",
-          }}
-        />
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
 
-        <Button onClick={{}}>Search</Button>
-      </InputGroup>
-    </Container>
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+    };
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
+
+  async function search() {
+    let artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    // Get Artist
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    console.log("Search Input: " + searchInput);
+    console.log("Artist ID: " + artistID);
+
+    // Get Artist Albums
+    await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums?include_groups=album&market=US&limit=50",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        setAlbums(data.items);
+      });
+  }
+
+  return (
+    <>
+      <Container>
+        <InputGroup>
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            aria-label="Search for an Artist"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+            style={{
+              width: "300px",
+              height: "35px",
+              borderWidth: "0px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              marginRight: "10px",
+              paddingLeft: "10px",
+            }}
+          />
+
+          <Button onClick={search}>Search</Button>
+        </InputGroup>
+      </Container>
+    </>
   );
 }
+
 export default App;
-
-function setSearch() {
-
-}
-
-function search() {
-
-}
